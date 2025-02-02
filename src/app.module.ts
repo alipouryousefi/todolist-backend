4563708ae3306c0bbd2c5b74077a88573b43a354
Ali Pouryousefi <a.pouryousefi98@gmail.com>
2025-02-02 17:39:58 +0330

    add configuration for environment variables and database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,38 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TodosModule } from './todos/todos.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import environmentValidation from './config/environment.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [AuthModule, TodosModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env.development' : `.env.development`,
+      load: [appConfig, databaseConfig],
+      // validationSchema: environmentValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        synchronize: configService.get('database.synchronize'),
+        port: configService.get('database.port'),
+        username: 'postgres',
+        password: 'password',
+        host: configService.get('database.host'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        database: configService.get('database.name'),
+      }),
+    }),
+    AuthModule,
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { registerAs } from '@nestjs/config';
+
+
+export default registerAs("database",()=>({
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT) || 5432,
+    user: process.env.DATABASE_USER ?? 'postgres',
+    password: process.env.DATABASE_PASSWORD ?? 'password',
+    name: process.env.DATABASE_NAME ?? 'chat',
+    synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
+    autoLoadEntities: process.env.DATABASE_AUTOLOAD === 'true' ? true : false,
+}))
